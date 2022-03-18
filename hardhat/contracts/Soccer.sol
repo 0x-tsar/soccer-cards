aed8d208ea3df0f607d74d40ae63d6c46081fac9
Gustavo <contato.gustavofernandes@hotmail.com>
2022-03-18 01:56:57 -0300

    version 0.0.11

--- hardhat/contracts/Soccer.sol
+++ hardhat/contracts/Soccer.sol
@@ -2,9 +2,13 @@
 
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+// import "@openzeppelin/contracts/utils/Counters.sol";
+
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
 
 contract Soccer is ERC721URIStorage {
     using Counters for Counters.Counter;
@@ -16,18 +20,51 @@ contract Soccer is ERC721URIStorage {
         uint256 id;
         uint256 createdAt;
         address owner;
+        address creator;
+        uint256 totalAmount;
     }
 
-    constructor() ERC721("Soccer Cards", "SOKR") {}
+    address payable immutable owner;
+    mapping(address => bool) public admins;
+    uint256 CONTRACT_FEE = 100; //1.0%
 
-    function mint(string memory tokenURI) public returns (uint256) {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
+    constructor() ERC721("Soccer Cards", "SOKR") {
+        owner = payable(msg.sender);
+    }
 
-        _mint(address(this), newItemId);
-        _setTokenURI(newItemId, tokenURI);
+    function mint(string memory tokenURI, uint256 amount) public {
+        require(
+            owner == msg.sender || admins[msg.sender],
+            "You are not allowed to mint a card, because you are not an admin or the owner"
+        );
 
-        return newItemId;
+        for (uint256 i = 0; i < amount; i++) {
+            _tokenIds.increment();
+            uint256 newItemId = _tokenIds.current();
+
+            _mint(address(this), newItemId);
+            _setTokenURI(newItemId, tokenURI);
+
+            //card creator
+            cardCreator[newItemId] = msg.sender;
+
+            CardStruct memory card = CardStruct({
+                id: newItemId,
+                createdAt: block.timestamp,
+                owner: address(this),
+                creator: msg.sender,
+                totalAmount: amount
+            });
+
+            cards[newItemId] = card;
+        }
+    }
+
+    // adding card creators, just admin allowed to add
+    function addCreator(address addr) external {
+        require(owner == msg.sender, "YOU ARE NOT ADMIN");
+        require(!admins[addr], "This address is an admin already!");
+        admins[addr] = true;
     }
 
     function buyCardFromMarket(uint256 id) public payable {}
@@ -38,3 +75,63 @@ contract Soccer is ERC721URIStorage {
         return (amount * CONTRACT_FEE) / 10000;
     }
 }
+
+//  function mintCards(
+//         string memory _title,
+//         uint256 _price,
+//         string memory _description,
+//         string memory _club,
+//         string memory _urlPicture,
+//         uint256 _totalAmount
+//     ) external {
+//         require(creators[msg.sender], "YOU ARE NOT A CREATOR");
+//         require(
+//             clubToCreator[_club] == msg.sender || admin == msg.sender,
+//             "YOU ARE NOT CREATOR OF THIS CLUB"
+//         );
+
+//         for (uint256 i = 0; i < _totalAmount; i++) {
+//             //
+//             Card memory card = Card({
+//                 title: _title,
+//                 id: nextItemId,
+//                 owner: address(this),
+//                 price: _price,
+//                 description: _description,
+//                 urlPicture: _urlPicture,
+//                 club: _club,
+//                 timestamp: block.timestamp,
+//                 createdBy: msg.sender,
+//                 totalAmount: _totalAmount
+//             });
+
+//             _mint(address(this), nextItemId.current());
+//             marketCards[address(this)][nextItemId.current()] = card;
+//             _setTokenURI(nextItemId.current(), _urlPicture);
+//             nextItemId.increment();
+
+//             //      _tokenIds.increment();
+
+//             // uint256 newItemId = _tokenIds.current();
+//             // _mint(owner(), newItemId);
+//             // _setTokenURI(newItemId, tokenURI);
+
+//             // _setTokenURI(nextItemId, _tokenURI);
+//             // _setTokenURI(nextItemId.current(), _urlPicture); //ipfs or pinata address
+
+//             emit cardMinted(
+//                 card.id,
+//                 card.owner,
+//                 card.timestamp,
+//                 card.title,
+//                 card.price,
+//                 card.description,
+//                 card.urlPicture,
+//                 card.totalAmount,
+//                 card.createdBy
+//             );
+//         }
+
+//         // tokenOfOwnerByIndex(owner, index);
+//         // tokenByIndex(index);
+//     }
