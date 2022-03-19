2476a6d7d12e627b7c109981f7abc28f4e09af58
Gustavo <contato.gustavofernandes@hotmail.com>
2022-03-19 14:22:19 -0300

    0.0.4 ready, displaying the right cards for main and market and buyMarket onclick working!

--- hardhat/scripts/script.js
+++ hardhat/scripts/script.js
@@ -4,20 +4,20 @@ const hh = require("hardhat");
 const SoccerContract = require("../artifacts/contracts/Soccer.sol/Soccer.json");
 
 async function main() {
-  // const SoccerContract = await hh.ethers.getContractFactory("Soccer");
+  const SoccerContract = await hh.ethers.getContractFactory("Soccer");
 
-  // const soccerContract = await SoccerContract.deploy();
-  // await soccerContract.deployed();
-  // console.log("Soccer Contract deployed to:", soccerContract.address);
+  const soccerContract = await SoccerContract.deploy();
+  await soccerContract.deployed();
+  console.log("Soccer Contract deployed to:", soccerContract.address);
 
-  const provider = new ethers.providers.getDefaultProvider(process.env.mumbai);
-  const signer = new ethers.Wallet(process.env.account, provider);
+  // const provider = new ethers.providers.getDefaultProvider(process.env.mumbai);
+  // const signer = new ethers.Wallet(process.env.account, provider);
 
-  const soccerContract = new ethers.Contract(
-    "0x5f4709Ae93ccda191fc7d7DE7dC4BBedAeF4aF30",
-    SoccerContract.abi,
-    signer
-  );
+  // const soccerContract = new ethers.Contract(
+  //   "0x5f4709Ae93ccda191fc7d7DE7dC4BBedAeF4aF30",
+  //   SoccerContract.abi,
+  //   signer
+  // );
 
   // console.log(await soccerContract);
 
@@ -33,11 +33,11 @@ async function main() {
 
   console.log(tx1);
 
-  // const tx2 = await soccerContract.mint(
-  //   "https://gateway.pinata.cloud/ipfs/QmaAKjJKhwUgwBnNfyyEFn2uFnYrHFjQfyrfufczKpJ6gk/luciano.json",
-  //   3,
-  //   Number(value)
-  // );
+  const tx2 = await soccerContract.mint(
+    "https://gateway.pinata.cloud/ipfs/QmaAKjJKhwUgwBnNfyyEFn2uFnYrHFjQfyrfufczKpJ6gk/luciano.json",
+    3,
+    Number(value)
+  );
   // console.log(tx2);
 
   // const tx3 = await soccerContract.mint(
